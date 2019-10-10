d161d01e04e7f7d330bc4eeec6fbf1d5abee1e8c
Dennis Alund <dennis.alund@gmail.com>
2019-10-10 17:16:31 +0800

    Testing some default code in swift

--- ios/Classes/SwiftFacebookAppEventsPlugin.swift
+++ ios/Classes/SwiftFacebookAppEventsPlugin.swift
@@ -3,12 +3,22 @@ import UIKit
 
 public class SwiftFacebookAppEventsPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "facebook_app_events", binaryMessenger: registrar.messenger())
+    let channel = FlutterMethodChannel(name: "flutter.oddbit.id/facebook_app_events", binaryMessenger: registrar.messenger())
     let instance = SwiftFacebookAppEventsPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    switch call.method {
+    case "getPlatformVersion":
+      handleGetPlatformVersion(call, result: result)
+      break;
+    default:
+      result(FlutterMethodNotImplemented)
+    }
+  }
+
+  private func handleGetPlatformVersion(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     result("iOS " + UIDevice.current.systemVersion)
   }
 }

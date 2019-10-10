87eea62fc5898990f58bcc51dfa8a9242b75902a
Dennis Alund <dennis.alund@gmail.com>
2019-10-11 00:30:20 +0800

    Updating the tests

--- test/facebook_app_events_test.dart
+++ test/facebook_app_events_test.dart
@@ -3,19 +3,37 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:facebook_app_events/facebook_app_events.dart';
 
 void main() {
-  const MethodChannel channel = MethodChannel('facebook_app_events');
+  const channel = MethodChannel(channelName);
+  final facebookAppEvents = FacebookAppEvents();
+  MethodCall methodCall;
 
-  setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
+  setUp(() async {
+    channel.setMockMethodCallHandler((MethodCall m) async {
+      methodCall = m;
     });
   });
 
   tearDown(() {
     channel.setMockMethodCallHandler(null);
+    methodCall = null;
   });
 
-  test('getPlatformVersion', () async {
-    expect(await FacebookAppEvents.platformVersion, '42');
+  group('Event logging', () {
+    test('logEvent log events', () async {
+      await facebookAppEvents.logEvent(
+        name: 'test-event',
+        parameters: <String, dynamic>{'a': 'b'},
+      );
+      expect(
+        methodCall,
+        isMethodCall(
+          'logEvent',
+          arguments: <String, dynamic>{
+            'name': 'test-event',
+            'parameters': <String, dynamic>{'a': 'b'},
+          },
+        ),
+      );
+    });
   });
 }

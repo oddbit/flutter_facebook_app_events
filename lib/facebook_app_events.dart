50a5637054f09e03d004a6c8e99fe1afe974dbd4
Dennis Alund <dennis.alund@gmail.com>
2019-10-12 10:19:52 +0800

    Adding comments to flutter public API

--- lib/facebook_app_events.dart
+++ lib/facebook_app_events.dart
@@ -8,14 +8,17 @@ const channelName = 'flutter.oddbit.id/facebook_app_events';
 class FacebookAppEvents {
   static const _channel = MethodChannel(channelName);
 
+  /// Clears the current user data
   Future<void> clearUserData() {
     return _channel.invokeMethod<void>('clearUserData');
   }
 
+  /// Clears the currently set user id.
   Future<void> clearUserID() {
     return _channel.invokeMethod<void>('clearUserID');
   }
 
+  /// Log an app event with the specified [name] and the supplied [parameters] value.
   Future<void> logEvent({
     @required String name,
     Map<String, dynamic> parameters,
@@ -30,6 +33,10 @@ class FacebookAppEvents {
     return _channel.invokeMethod<void>('logEvent', args);
   }
 
+  /// Sets user data to associate with all app events. 
+  /// All user data are hashed and used to match Facebook user from this 
+  /// instance of an application. The user data will be persisted between 
+  /// application instances.
   Future<void> setUserData({
     String email,
     String firstName,
@@ -58,10 +65,15 @@ class FacebookAppEvents {
     return _channel.invokeMethod<void>('setUserData', args);
   }
 
+  /// Sets a user [id] to associate with all app events. 
+  /// This can be used to associate your own user id with the 
+  /// app events logged from this instance of an application. 
+  /// The user ID will be persisted between application instances.
   Future<void> setUserID(String id) {
     return _channel.invokeMethod<void>('setUserID', id);
   }
 
+  /// Update user properties as provided by a map of [parameters]
   Future<void> updateUserProperties({
     @required Map<String, dynamic> parameters,
     String applicationId,

f19aae801bceaca71602b902688194fcd8100037
Dennis Alund <dennis.alund@gmail.com>
2020-11-17 11:38:08 +0800

    Updating iOS integration

--- ios/Classes/FacebookAppEventsPlugin.m
+++ ios/Classes/FacebookAppEventsPlugin.m
@@ -1,5 +1,12 @@
 #import "FacebookAppEventsPlugin.h"
+#if __has_include(<facebook_app_events/facebook_app_events-Swift.h>)
 #import <facebook_app_events/facebook_app_events-Swift.h>
+#else
+// Support project import fallback if the generated compatibility header
+// is not copied when this plugin is created as a library.
+// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
+#import "facebook_app_events-Swift.h"
+#endif
 
 @implementation FacebookAppEventsPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {

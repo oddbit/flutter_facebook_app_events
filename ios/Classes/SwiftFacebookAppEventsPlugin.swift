35fe69e5b5d5f390ce236c6f1b5790eab567b1e7
Parama Dharmika <eka@thnx.se>
2021-11-10 02:22:35 +0700

    Add FBAudienceNetwork dependency and fix parameters arg casting compilation error on handle log event

--- ios/Classes/SwiftFacebookAppEventsPlugin.swift
+++ ios/Classes/SwiftFacebookAppEventsPlugin.swift
@@ -86,7 +86,7 @@ public class SwiftFacebookAppEventsPlugin: NSObject, FlutterPlugin {
     private func handleLogEvent(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
         let arguments = call.arguments as? [String: Any] ?? [String: Any]()
         let eventName = arguments["name"] as! String
-        let parameters = arguments["parameters"] as? [AppEvents.ParameterName: Any] ?? [AppEvents.ParameterName: Any]()
+        let parameters = arguments["parameters"] as? [String: Any] ?? [String: Any]()
         if arguments["_valueToSum"] != nil && !(arguments["_valueToSum"] is NSNull) {
             let valueToDouble = arguments["_valueToSum"] as! Double
             AppEvents.logEvent(AppEvents.Name(eventName), valueToSum: valueToDouble, parameters: parameters)

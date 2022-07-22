85d166358a432c90d206554747b95b6e96bb1d32
hifiaz <fiazhari@gmail.com>
2022-07-22 09:52:24 +0700

    added connection of AppDelegate & sdk

--- ios/Classes/SwiftFacebookAppEventsPlugin.swift
+++ ios/Classes/SwiftFacebookAppEventsPlugin.swift
@@ -15,6 +15,26 @@ public class SwiftFacebookAppEventsPlugin: NSObject, FlutterPlugin {
         ApplicationDelegate.shared.initializeSDK()
 
         registrar.addMethodCallDelegate(instance, channel: channel)
+        registrar.addApplicationDelegate(instance)
+     }
+
+     /// Connect app delegate with SDK
+     public func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [AnyHashable : Any] = [:]) -> Bool {
+         var options = [UIApplication.LaunchOptionsKey: Any]()
+         for (k, value) in launchOptions {
+             let key = k as! UIApplication.LaunchOptionsKey
+             options[key] = value
+         }
+         ApplicationDelegate.shared.application(application,didFinishLaunchingWithOptions: options)
+         return true
+     }
+
+     public func application( _ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:] ) -> Bool {
+         let processed = ApplicationDelegate.shared.application(
+             app, open: url,
+             sourceApplication: options[UIApplication.OpenURLOptionsKey.sourceApplication] as? String,
+             annotation: options[UIApplication.OpenURLOptionsKey.annotation])
+         return processed;
     }
 
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
